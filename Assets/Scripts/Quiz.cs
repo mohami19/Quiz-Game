27d726d897a2d98fa72b153d445a8bda8b35e219
Mohammad Mahdi Gholami <gholamimahdi680@gmail.com>
2024-06-10 12:27:51 +0330

    Add Logic for Clinking Buttons

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+
 public class Quiz : MonoBehaviour
 {
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] GameObject[] answerButtons;
+    int correctAnswer;
+    [SerializeField] Sprite defaultAnswerSprit;
+    [SerializeField] Sprite correctAnswerSprit;
     void Start()
     {
+        DisplayQuestion();
+    }
+
+    public void onAnswerSelected(int index){
+        correctAnswer = question.GetCorrectAnswer();
+        Image ButtonImage = answerButtons[correctAnswer].GetComponent<Image>();
+        if (index == correctAnswer){
+            questionText.text = "correct";
+            ButtonImage.sprite = correctAnswerSprit;
+        } else {
+            ButtonImage.sprite = correctAnswerSprit;
+            // string correctAnswerText = question.GetAnswers(correctAnswer);
+            questionText.text = "The Correct Answer Has Been Highlighted";
+        }
+    }
+
+    void DisplayQuestion(){
         questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; i++) {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswers(i);
+        }
     }
 
 }
